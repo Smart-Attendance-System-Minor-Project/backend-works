07cfa1a703c5359399e09ebe663ee44f82cf9ca4
Pratik Dahal <076bct051.pratik@pcampus.edu.np>
2023-02-06 18:41:20 +0545

    fixed minor bugs

--- ProjectBackend/AttendanceApp/validation.py
+++ ProjectBackend/AttendanceApp/validation.py
@@ -1,17 +1,19 @@
-import ast
+import ast, json
 
 teacher_email_list = ['pratikdahal05@gmail.com', 'imrajeshneupane@gmail.com', 'jharahul968@gmail.com']
 
 def returnPresence(record):
-    attendance_record = ast.literal_eval(record)
-    total_students = len(attendance_record)
+    print(f"record = {record} and type(record) = {type(record)}")
+    # attendance_record = json.dumps(record)
+    # print(f"attendance_record = {attendance_record} and type(attendance_record) = {type(attendance_record)}")
+    total_students = len(record)
     total_presence = 0
-    for i in attendance_record:
-        if i['presence'] == 'present':
+    for i in record.values():
+        if i.lower() == 'p':
             total_presence += 1
     
     try:
-        percentage_present = (total_presence/total_students)*100
+        percentage_present = round((total_presence/total_students)*100, 2)
         return percentage_present
     
     except Exception as e:
