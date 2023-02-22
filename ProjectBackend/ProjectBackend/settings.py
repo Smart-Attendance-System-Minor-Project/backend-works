2b6d9a6ba419f7015fad509d65ad9beb25acb527
Pratik Dahal <076bct051.pratik@pcampus.edu.np>
2023-02-22 21:38:16 +0545

    CORS access given to localhost

--- ProjectBackend/ProjectBackend/settings.py
+++ ProjectBackend/ProjectBackend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:3000',
+  'http://172.17.80.1:3000',
+)
+
 
 ROOT_URLCONF = 'ProjectBackend.urls'
 
