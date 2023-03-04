534092c2c3980f8e0b34c8fbdbbfc3e92fbdf0fe
Pratik Dahal <076bct051.pratik@pcampus.edu.np>
2023-03-04 21:16:49 +0545

    email verification added while registration

--- ProjectBackend/AttendanceApp/models.py
+++ ProjectBackend/AttendanceApp/models.py
@@ -5,7 +5,7 @@ from .managers import TeacherManager
 # Create your models here.
 class Teacher(AbstractUser):
     username = models.CharField(max_length = 20, unique= True)
-    email = models.EmailField(max_length=40,unique=False)
+    email = models.EmailField(max_length=40, unique=True)
     # password = models.CharField(max_length= 30)
     full_name = models.CharField(max_length=40)
     classes = models.CharField(max_length= 10000)
