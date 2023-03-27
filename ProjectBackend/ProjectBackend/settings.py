654d52913e518facd2a980e18baf4f9b561916b3
Pratik Dahal <prat@pop-os.localdomain>
2023-03-27 11:36:14 +0545

    sensitive information removed

--- ProjectBackend/ProjectBackend/settings.py
+++ ProjectBackend/ProjectBackend/settings.py
@@ -174,7 +174,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mail.ioehub@gmail.com'
-EMAIL_HOST_PASSWORD = 'qiluuhbznkfcphiq'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
